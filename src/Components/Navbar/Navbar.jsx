6f4eb297ab9b320cf6c7e70e4fb8316a2aabb95d
imjoyhalder <joyhalder00113355@gmail.com>
2025-05-15 02:25:46 +0600

    added logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <img className=' w-12 h-12 rounded-full' src="GadgetZone Logo - Gradient Design without Text.png" alt="" />
                 </div>
                 <div className="flex gap-2">
                     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
