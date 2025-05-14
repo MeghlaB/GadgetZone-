8919e9f96cc967c858786edd17e0efbc36b2694e
imjoyhalder <joyhalder00113355@gmail.com>
2025-05-14 23:46:27 +0600

    daisyUl and tailwind css and firebase setup done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAanscVR13fSQOTJVwZXjUPUTfNzethsVw",
+  authDomain: "gadzetzone-be819.firebaseapp.com",
+  projectId: "gadzetzone-be819",
+  storageBucket: "gadzetzone-be819.firebasestorage.app",
+  messagingSenderId: "307832102252",
+  appId: "1:307832102252:web:f44ca65f6daaf556aae749"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
