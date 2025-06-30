8619fa22b21303b4dec1994ada61c9bdfbdf5d7d
imjoyhalder <joyhalder00113355@gmail.com>
2025-06-30 23:15:26 +0600

    Drone category added

--- src/Components/FeaturedCategory/SingleFeaturedCategory.jsx
+++ src/Components/FeaturedCategory/SingleFeaturedCategory.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SingleFeaturedCategory = ({ category }) => {
+    console.log(category)
     return (
-        <div className="w-24 h-24 md:w-32 md:h-32 flex flex-col items-center justify-center text-center bg-base-100 shadow-2xl md:p-4 rounded-2xl">
+        <div className="flex flex-col items-center justify-center w-24 h-24 text-center shadow-2xl md:w-32 md:h-32 bg-base-100 md:p-4 rounded-2xl">
             <Link className='flex flex-col items-center' to={`/category/${category.name.toLowerCase().replace(/\s+/g, '-')}`}>
                 <div className="w-14 h-14 md:w-20 md:h-20 ">
-                    <img src={category.icon} alt={category.name} className="w-full h-full object-contain" />
+                    <img src={category.icon} alt={category.name} className="object-contain w-full h-full" />
                 </div>
                 <p className="text-sm font-medium">{category.name}</p>
             </Link>
